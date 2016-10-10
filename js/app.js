468a7a0090a026408ed3d9adf525b75a9885fb26
Jaemin Han <jaemin,han@nyu.edu>
2016-10-10 13:09:18 -0400

    Syntax organization and layout & obstacles positioning

--- js/app.js
+++ js/app.js
@@ -24,72 +24,73 @@ var $boxes = function () {
 
 $boxes();
 
+
 //creating a player class under the container div
 var $player = $('<div class="player"></div>');
 var $gif_img = $('<img src="img/playernew.gif">');
-  $player.append($gif_img);
-  $container.append($player);
+$player.append($gif_img);
+$container.append($player);
 
 
 // Create arrow movements for player
-  $('body').keydown(function(move) {
+$('body').keydown(function(move) {
+  var position = $('.player').position();
 
-    var position = $('.player').position();
-
-    switch (move.keyCode)
-    {
-      case 37:
-        $('.player').css('left', position.left - 20 + 'px');
-        divCollide($player, $obstacles);
-        win($player, $winning);
-        break;
-
-      case 38:
-        $('.player').css('top', position.top - 20 + 'px');
-        divCollide($player, $obstacles);
-        win($player, $winning);
-        break;
-
-      case 39:
-        $('.player').css('left', position.left + 20 + 'px');
-        divCollide($player, $obstacles);
-        win($player, $winning);
-        break;
-
-      case 40:
-        $('.player').css('top', position.top + 20 + 'px');
-        divCollide($player, $obstacles);
-        win($player, $winning);
-        break;
-    }
-  });
+  switch (move.keyCode)
+  {
+    case 37:
+      $('.player').css('left', position.left - 20 + 'px');
+      divCollide($player, $obstacles);
+      win($player, $winning);
+      break;
+    case 38:
+      $('.player').css('top', position.top - 20 + 'px');
+      divCollide($player, $obstacles);
+      win($player, $winning);
+      break;
+    case 39:
+      $('.player').css('left', position.left + 20 + 'px');
+      divCollide($player, $obstacles);
+      win($player, $winning);
+      break;
+    case 40:
+      $('.player').css('top', position.top + 20 + 'px');
+      divCollide($player, $obstacles);
+      win($player, $winning);
+      break;
+  }
+});
 
 
-//randomize obstacles at [1] different starting speed and [2] movement speed
+//randomize obstacles at
+//1. different starting speed
+//2. movement speed
 function getRandom(a, b) {
   return (Math.random()*a)+b;
 }
 
-  var $obstacles =$(".obstacles");
-  var width = $obstacles.get(0).width;
-  var screenWidth = $(".flex-container").width();
-  var duration = getRandom(3500, 1000);
+var $obstacles =$(".obstacles");
+var width = $obstacles.get(0).width;
+var screenWidth = $(".flex-container").width();
+var duration = getRandom(3500, 1000);
 
 
-  function animateObsticles() {
-    $obstacles.eq(0).css("left", -width).delay(getRandom(2000, 100)).animate({
-    "left": screenWidth}, duration, animateObsticles);
+function animateObsticles() {
+  $obstacles.eq(0).css("left", -width).delay(getRandom(2000, 100)).animate({
+  "left": screenWidth}, duration, animateObsticles);
 
-    $obstacles.eq(1).css("left", -width).delay(getRandom(3000, 1000)).animate({
-    "left": screenWidth}, duration, animateObsticles);
+  $obstacles.eq(1).css("left", -width).delay(getRandom(3000, 1000)).animate({
+  "left": screenWidth}, duration, animateObsticles);
 
-    $obstacles.eq(2).css("left", -width).delay(getRandom(2000, 1000)).animate({
-    "left": screenWidth}, duration, animateObsticles);
-    }
+  $obstacles.eq(2).css("left", -width).delay(getRandom(2000, 1000)).animate({
+  "left": screenWidth}, duration, animateObsticles);
+  }
 
-  animateObsticles();
+animateObsticles();
+
 
 // Trying to loop obstacles left to right
+//Below are list of obstacles with different speed and location
 function leftToRight(){
   var $right = $('.right');
   var location = -50;
@@ -103,7 +104,7 @@ function leftToRight(){
   }, 100);
 };
 
-  leftToRight();
+leftToRight();
 
 function leftToRight1(){
   var $right1 = $('.right1');
@@ -118,7 +119,7 @@ function leftToRight1(){
   }, 100);
 };
 
-  leftToRight1();
+leftToRight1();
 
 function leftToRight2(){
   var $ob8 = $('#obstacle8');
@@ -133,7 +134,7 @@ function leftToRight2(){
   }, 100);
 };
 
-  leftToRight2();
+leftToRight2();
 
 function leftToRight3(){
   var $ob8a = $('#obstacle8a');
@@ -148,7 +149,7 @@ function leftToRight3(){
   }, 100);
 };
 
-  leftToRight3();
+leftToRight3();
 
 function leftToRight4(){
   var $ob8b = $('#obstacle8b');
@@ -163,7 +164,7 @@ function leftToRight4(){
   }, 100);
 };
 
-  leftToRight4();
+leftToRight4();
 
 function leftToRight5(){
   var $ob8c = $('#obstacle8c');
@@ -178,7 +179,7 @@ function leftToRight5(){
   }, 100);
 };
 
-  leftToRight5();
+leftToRight5();
 
 
 // Trying to move obstacles from right to left
@@ -195,11 +196,11 @@ function rightToLeft(){
   }, 100);
 };
 
-  rightToLeft();
+rightToLeft();
 
-  function rightToLeft1(){
-    var $ob5 = $('#obstacle5');
-    var distance = 800;
+function rightToLeft1(){
+  var $ob5 = $('#obstacle5');
+  var distance = 800;
   setInterval(function(){
     $ob5.css('left', distance + 'px');
     if (distance < -50) {
@@ -210,41 +211,41 @@ function rightToLeft(){
   }, 100);
 };
 
-  rightToLeft1();
+rightToLeft1();
 
-  function rightToLeft2(){
-    var $ob7a = $('#obstacle7a');
-    var distance = 400;
+function rightToLeft2(){
+  var $ob7a = $('#obstacle7a');
+  var distance = 400;
   setInterval(function(){
     $ob7a.css('left', distance + 'px');
     if (distance < -50) {
       distance = 1200;
     } else {
-      distance -= 40;
+      distance -= 10;
     }
   }, 100);
 };
 
-  rightToLeft2();
+rightToLeft2();
 
-  function rightToLeft3(){
-    var $ob7b = $('#obstacle7b');
-    var distance = 900;
+function rightToLeft3(){
+  var $ob7b = $('#obstacle7b');
+  var distance = 900;
   setInterval(function(){
     $ob7b.css('left', distance + 'px');
     if (distance < -50) {
       distance = 1200;
     } else {
-      distance -= 30;
+      distance -= 25;
     }
   }, 100);
 };
 
-  rightToLeft3();
+rightToLeft3();
 
-  function rightToLeft4(){
-    var $ob7c = $('#obstacle7c');
-    var distance = 400;
+function rightToLeft4(){
+  var $ob7c = $('#obstacle7c');
+  var distance = 400;
   setInterval(function(){
     $ob7c.css('left', distance + 'px');
     if (distance < -50) {
@@ -255,107 +256,107 @@ function rightToLeft(){
   }, 100);
 };
 
-  rightToLeft4();
+rightToLeft4();
 
-  function rightToLeft5(){
-    var $ob7d = $('#obstacle7d');
-    var distance = 350;
+function rightToLeft5(){
+  var $ob7d = $('#obstacle7d');
+  var distance = 350;
   setInterval(function(){
     $ob7d.css('left', distance + 'px');
+    if (distance < -60) {
+      distance = 1250;
+    } else {
+      distance -= 55;
+    }
+  }, 100);
+};
+
+rightToLeft5();
+
+function rightToLeft6(){
+  var $ob7e = $('#obstacle7e');
+  var distance = 190;
+  setInterval(function(){
+    $ob7e.css('left', distance + 'px');
+    if (distance < -55) {
+      distance = 1100;
+    } else {
+      distance -= 45;
+    }
+  }, 100);
+};
+
+rightToLeft6();
+
+function rightToLeft7(){
+  var $ob7f = $('#obstacle7f');
+  var distance = 190;
+  setInterval(function(){
+    $ob7f.css('left', distance + 'px');
     if (distance < -50) {
-      distance = 1200;
+      distance = 1250;
     } else {
       distance -= 35;
     }
   }, 100);
 };
 
-  rightToLeft5();
-
-  function rightToLeft6(){
-    var $ob7e = $('#obstacle7e');
-    var distance = 190;
-  setInterval(function(){
-    $ob7e.css('left', distance + 'px');
-    if (distance < -50) {
-      distance = 1100;
-    } else {
-      distance -= 45;
-    }
-  }, 100);
-};
-
-  rightToLeft6();
-
-  function rightToLeft7(){
-    var $ob7f = $('#obstacle7f');
-    var distance = 190;
-  setInterval(function(){
-    $ob7f.css('left', distance + 'px');
-    if (distance < -50) {
-      distance = 1100;
-    } else {
-      distance -= 45;
-    }
-  }, 100);
-};
-
-  rightToLeft7();
-
+rightToLeft7();
 
 
 //Collide detaction on all obstacles when the player moves
 //Detection based on movement 'left' 'up' 'right' 'down'
 //Assistance from Matt Keigwin to make the detection
 //Created custom box to set alert, text, and reload
-  var $player = $('.player');
-  var $obstacles = $('.obstacles');
+var $player = $('.player');
+var $obstacles = $('.obstacles');
 
-
-  function divCollide($player, $obstacles) {
-    var playerTop = $player.offset().top;
-    var playerLeft = $player.offset().left;
-    var playerRight = Number($player.offset().left) + Number($player.width());
-    var playerBottom = Number($player.offset().top) + Number($player.height());
-    var obstacleTop = $obstacles.offset().top;
-    var obstacleLeft = $obstacles.offset().left;
-    var obstacleRight = Number($obstacles.offset().left) + Number($obstacles.width());
-    var obstacleBottom = Number($obstacles.offset().top) + Number($obstacles.height());
-    if (playerRight > obstacleLeft && playerLeft < obstacleRight && playerTop < obstacleBottom && playerBottom > obstacleTop) {
-      alert("Shoot~! Next Time look left and right before crossing.")
-      location.reload();
-    }
+function divCollide($player, $obstacles) {
+  var playerTop = $player.offset().top;
+  var playerLeft = $player.offset().left;
+  var playerRight = Number($player.offset().left) + Number($player.width());
+  var playerBottom = Number($player.offset().top) + Number($player.height());
+  var obstacleTop = $obstacles.offset().top;
+  var obstacleLeft = $obstacles.offset().left;
+  var obstacleRight = Number($obstacles.offset().left) + Number($obstacles.width());
+  var obstacleBottom = Number($obstacles.offset().top) + Number($obstacles.height());
+  if (playerRight > obstacleLeft && playerLeft < obstacleRight && playerTop < obstacleBottom && playerBottom > obstacleTop) {
+    alert("Shoot~! Next Time look left and right before crossing.")
+    location.reload();
   }
+}
 
-  setInterval(function() {
-    $obstacles.each(function() {
-      divCollide($player,$(this));
-    })
-  }, 100);
+setInterval(function() {
+  $obstacles.each(function() {
+    divCollide($player,$(this));
+  })
+}, 100);
+
 
 // If a player reaches the bottom of the page - alert occurs
-  var $winning = $('.finish');
-  var score = $('#score');
-  var gameScore = 0;
-  // var score_updated = false;
+var $winning = $('.finish');
+var score = $('#score');
+var gameScore = 0;
 
-  function win($player, $winning){
-    var playerBottom = Number($player.offset().top) + Number($player.height());
-    var finishBottom = Number($winning.offset().top) + Number($winning.height());
-    if(playerBottom > finishBottom) {
+function win($player, $winning){
+  var playerBottom = Number($player.offset().top) + Number($player.height());
+  var finishBottom = Number($winning.offset().top) + Number($winning.height());
+  if(playerBottom > finishBottom) {
 
-      gameScore+=1;
-      $('#score').text(gameScore);
-      scorePlus();
+    gameScore+=1;
+    $('#score').text(gameScore);
+    scorePlus();
 
-    }
   }
-  win($player, $winning);
+}
+
+win($player, $winning);
+
 
 // Adding refresh with score plus plus
- function scorePlus(){
+function scorePlus(){
   $player.offset({top: 0, left: 650}).top = 0;
- }
+}
 
 
 //Please note -- this code is from our group's project [WDI w02-d03 lab]
@@ -363,6 +364,7 @@ function rightToLeft(){
 //I manipulated the code to fit in my code
 // function courtesy of http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
 var players;
+
 (window.onpopstate = function () {
   var match,
     pl     = /\+/g,  // Regex for replacing addition symbol with a space
@@ -400,7 +402,5 @@ function playerName (itemObj) {
 }
 
 playerName(players);
-
-
 
 });
